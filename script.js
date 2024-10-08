95392b4577b888bbb7b05055f1ea63151ecde620
XzeelKetche <raihanf432@gmail.com>
2024-10-08 19:11:24 +0700

    Added credit & watermark

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/**
+ __  __              _ _  __    _       _          
+ \ \/ / _______  ___| | |/ /___| |_ ___| |__   ___ 
+  \  / |_  / _ \/ _ \ | ' // _ \ __/ __| '_ \ / _ \
+  /  \  / /  __/  __/ | . \  __/ || (__| | | |  __/
+ /_/\_\/___\___|\___|_|_|\_\___|\__\___|_| |_|\___|
+ * @INFO
+ * Source code by | XzeelKetche
+ * @INFO
+ * Source code ID | Portofolio-Website
+ * @INFO
+ * Perhatian      | Credit & watermark jangan pernah di hapus!
+ * @INFO
+*/
+
 var typed = new Typed(".multiple-text", {
 	strings:
 	["Student","Hacker","Programmer","Dreamer","Gamer"],
